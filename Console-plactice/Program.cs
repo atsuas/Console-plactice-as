403746a77e582d7598f8280c7b692737213bc988
as <aoyuu.920.96@gmail.com>
2021-04-19 08:39:51 +0900

    test

--- Console-plactice/Program.cs
+++ Console-plactice/Program.cs
@@ -6,7 +6,7 @@ namespace Console_plactice
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("aaaaaaaaa");
         }
     }
 }
