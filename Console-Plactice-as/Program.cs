fad9c6de2aa41c2d072f74b4d444c5a5564f5613
as <>
2021-09-19 22:20:17 +0900

    プロジェクト作成

---
+++ Console-Plactice-as/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Console_Plactice_as
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
