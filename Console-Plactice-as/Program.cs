3e0549fdee1f0fb8670e5d4f337ab2a4c5eaacb9
as <>
2021-09-19 22:28:37 +0900

    動作確認

--- Console-Plactice-as/Program.cs
+++ Console-Plactice-as/Program.cs
@@ -6,7 +6,7 @@ namespace Console_Plactice_as
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World");
         }
     }
 }
